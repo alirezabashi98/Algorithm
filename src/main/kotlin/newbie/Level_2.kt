d0765966f3fd20ef2e1b4605d7e9791478445b21
alirezabai98 <alirezabashi98@gmail.com>
2021-01-14 20:05:29 +0330

    end level 2

---
+++ src/main/kotlin/newbie/Level_2.kt
@@ -0,0 +1,23 @@
+package newbie
+
+fun Level_2() {
+
+    var letter: Char = 'A'
+    var name: String = "alireza"
+    val age1: Byte = 18
+    val age2: Short = 18
+    val age3: Int = 18
+    val age4: Long = 18L
+
+    val width1: Float = 15.5f
+    val width2: Double = 15.5
+
+    val dark: Boolean = true
+
+    val id = arrayListOf(1, 2, 3, 4, 5, 6)
+    val firstId = id[0]
+    val lasted = id[id.size - 1]
+
+    val asc = Array(5, { i -> i * 2 })
+
+}
